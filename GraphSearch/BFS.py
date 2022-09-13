a4a7d3e6830382307d2d59ec37a6803c22f01e76
NormanZ <normanzheng6606@gmail.com>
2022-09-13 18:54:15 +0800

    default

--- GraphSearch/BFS.py
+++ GraphSearch/BFS.py
@@ -1,2 +1,25 @@
 # Use BFS algorithm to solve easy traffic cost problem
-print("Hello world")
+graph = {
+    "A" : {"B": 1, "C": 3},
+    "B" : {"A": 1, "D": 3, "E": 5},
+    "C" : {"A": 3, "H": 2},
+    "D" : {"B": 3, "F": 4},
+    "E" : {"B": 5, "G": 2, "H":}
+
+}
+visited = []
+queue = []
+
+def bfs(visited, traffic_map, city):
+    visited.append(city)
+    queue.append(city)
+
+    while queue:
+        m = queue.pop(0)
+
+        for neighbour in traffic_map[m]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
+
+
