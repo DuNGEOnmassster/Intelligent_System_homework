076b9e075a50a8aa7f0b944522c3ca45da5f2699
NormanZ <normanzheng6606@gmail.com>
2022-09-29 16:08:09 +0800

    update ex1

---
+++ ex1/8queen.py
@@ -0,0 +1,15 @@
+from audioop import maxpp
+import numpy as np
+from dataclasses import dataclass
+
+def init():
+    mapx = np.zeros((8,8))
+    print(mapx)
+    
+
+def A_star():
+    pass
+
+if __name__ == "__main__":
+    init()
+
