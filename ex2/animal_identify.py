e18295b859219fd565acad4ae0ff7172060534c8
NormanZ <1275133384@qq.com>
2022-10-08 16:33:32 +0800

    update rules-extending function

--- ex2/animal_identify.py
+++ ex2/animal_identify.py
@@ -1,4 +1,6 @@
+from json import load
 import os
+from turtle import pd
 import numpy as np
 from itertools import combinations
 from rules import init_rules, Point
@@ -8,11 +10,11 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser(description="Animal identify system")
 
-    parser.add_argument("-i","--input", default="./dataset/test.txt",
-                        help="input test sentense for identification")
+    parser.add_argument("-t","--test", default="./dataset/test.txt",
+                        help="test data sentense for identification")
     parser.add_argument("--extend", default=False,
                         help="whether use extend rules or not, default False")
-    parser.add_argument("--extend_input", default="./dataset/extend_rules.txt",
+    parser.add_argument("--extend_rules", default="./dataset/extend_rules.txt",
                         help="input file to extend rules.")
 
     return parser.parse_args()
@@ -35,14 +37,22 @@ def value2key(dict, value):
 
 
 def get_test_pattern(text, args):
-    datasets, emissions, targets = init_rules(args.extend)
+    if args.extend:
+        print("Use extend rules")
+        rules_text,_ = load_file(args.extend_rules)
+        datasets, emissions, targets = get_rule_pattern(rules_text)
+    else:
+        datasets, emissions, targets = init_rules()
+
     conditions = []
     init_split = text.split(sep="\n")
     for sentence in init_split:
         sen_list = sentence.split("：")
-        if len(sen_list) == 2: # Capable to resist the distraction of useless message
+        # Capable to resist the distraction of useless message
+        if len(sen_list) == 2: 
             condition = sen_list[1].split("某动物")[-1]
-            if condition in datasets.values(): # Avoid IndexError from fault condition
+            # Avoid IndexError from fault condition
+            if condition in datasets.values(): 
                 rule = value2key(datasets, condition)
                 # print(f"{rule}:{condition}")
                 conditions.append(rule)
@@ -50,27 +60,36 @@ def get_test_pattern(text, args):
     return conditions
 
 
-def get_rule_pattern(text, args):
-    datasets, emissions, targets = init_rules(False)
+def get_rule_pattern(text):
+    datasets, emissions, targets = init_rules()
     init_split = text.split(sep="\n")
     for sentence in init_split:
         sen_list = sentence.split(sep="：")
-        if len(sen_list) == 2: # Capable to resist the distraction of useless message
+        # Capable to resist the distraction of useless message
+        if len(sen_list) == 2: 
             result = sen_list[0].split(sep="）")[-1]
             condition_list = sen_list[1][:-1].split(sep="，")
-            print(f"{condition_list} result={result}")
+            # print(f"{condition_list} result={result}")
+
             # Attend new conditions and results into datasets
             for item in condition_list:
                 if item not in datasets.values():
-                    print(f"{item} not in datasets")
-                    print(len(datasets))
+                    # print(f"{item} not in datasets")
                     datasets[len(datasets)+1] = item
             if result not in datasets.values():
-                print(f"{result} not in datasets")
+                # print(f"{result} not in datasets")
                 datasets[len(datasets)+1] = result
+                # Attend new result into targets
+                targets.append(value2key(datasets, result))
 
-    print(datasets)
+            # Attend new emission into emissions
+            condition_number = []
+            for condition in condition_list:
+                condition_number.append(value2key(datasets, condition))
+            emissions.append(Point(set(condition_number),value2key(datasets, result)))
+
     return datasets, emissions, targets
+
 
 def get_permutations(conditions):
     perm = []
@@ -97,7 +116,7 @@ def get_combinations(conditions):
 
 
 def search(conditions, args):
-    datasets, emissions, targets = init_rules(args.extend)
+    datasets, emissions, targets = init_rules()
     print(f"conditions = {conditions}")
     # find the every condition combination to get results, put result as new condition
     comb = get_combinations(conditions)
@@ -123,18 +142,15 @@ def search(conditions, args):
                     print(f"new conditions is {conditions}")
                     # import pdb; pdb.set_trace()
             else:
-                # print(f"cnt:{cnt}\n{item.rules} Not in {comb} or in {visited}")
                 cnt = cnt + 1
     
 
 def find_rules():
     args = parse_args()
-    text, is_file = load_file(args.input)
+    text, is_file = load_file(args.test)
     print(f"It is {is_file} that this is a file")
     conditions = get_test_pattern(text, args)
-    # search(conditions, args)
-    extend_text, isfile = load_file(args.extend_input)
-    _,_,_ = get_rule_pattern(extend_text, args)
+    search(conditions, args)
 
 
 if __name__ == "__main__":
