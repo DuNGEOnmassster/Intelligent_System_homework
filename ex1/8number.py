cd5f18a58c349bc78b045238e42cce7d359dbb69
NormanZ <1275133384@qq.com>
2022-10-12 01:02:14 +0800

    update 8 number init process

--- ex1/8number.py
+++ ex1/8number.py
@@ -5,26 +5,44 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser(description="Animal identify system")
 
-    parser.add_argument("-t","--test", default="./dataset/test.txt",
-                        help="test data sentense for identification")
-    parser.add_argument("--extend", default=False,
-                        help="whether use extend rules or not, default False")
-    parser.add_argument("--extend_rules", default="./dataset/extend_rules.txt",
-                        help="input file to extend rules.")
+    parser.add_argument("--diy_start", default=False,
+                        help="judge whether to diy start 8 number map or not, default with False")
+    parser.add_argument("--start", default=[i for i in range(9)],
+                        help="design the start 8 number map")
+    parser.add_argument("--diy_target", default=False,
+                        help="judge whether to diy target 8 number map or not, default with False")
+    parser.add_argument("--target", default=[0, 1, 2, 7, 8, 3, 6, 5, 4],
+                        help="design the start 8 number map")
 
     return parser.parse_args()
 
 
-def init_board():
-    return np.array([i for i in range(9)]).reshape([3,3])
+def get_start(args):
+    if not args.diy_start:
+        return np.array(args.start).reshape([3,3])
+    else:
+        arr = []
+        for i in range(9):
+            arr.append(input(f"input number in start map {i}/9"))
+            return np.array(args.arr).reshape([3,3])
 
 
-def get_target():
-    target = [0, 1, 2, 7, 8, 3, 6, 5, 4]
-    return np.array(target).reshape([3,3])
+def get_target(args):
+    if not args.diy_target:
+        return np.array(args.target).reshape([3,3])
+    else:
+        arr = []
+        for i in range(9):
+            arr.append(input(f"input number in targer map {i}/9"))
+            return np.array(args.arr).reshape([3,3])
+
+
+def init():
+    args = parse_args()
+    start = get_start(args)
+    target = get_target(args)
+    print(f"start:\n{start}\ntarget:\n{target}")
 
 
 if __name__ == "__main__":
-    start = init_board()
-    target = get_target()
-    print(f"start:\n{start}\ntarget:\n{target}")
+    init()
