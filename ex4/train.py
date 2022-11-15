ddf3bb3db22a0f8a42bf65c974793848a01fc146
NormanZ <1275133384@qq.com>
2022-11-15 20:49:56 +0800

    update utils to get real split valid dataset and test dataset

--- ex4/train.py
+++ ex4/train.py
@@ -21,10 +21,10 @@ def parse_args():
 
     parser.add_argument("--batch_size_train", type=int, default=64,
                         help="declare batch size of train_loader")
-    parser.add_argument("--batch_size_valid", type=int, default=1000,
-                        help="declare batch size of valid_loader")
-    parser.add_argument("--batch_size_test", type=int, default=16,
+    parser.add_argument("--batch_size_test", type=int, default=1000,
                         help="declare batch size of test_loader")
+    parser.add_argument("--valid_split", type=float, default=0.5,
+                        help="declare proportion of valid in test_loader split, default with (valid = test)")
     parser.add_argument("--random_seed", type=int, default=3407,
                         help="use this magical random seed 3407")
     parser.add_argument("--lr", type=float, default=0.01,
--- ex4/utils/utils.py
+++ ex4/utils/utils.py
@@ -1,9 +1,10 @@
 import torch
 import torchvision
-
+from torch.utils import data as Data
 
 def init_dataloader(args):
     torch.manual_seed(args.random_seed)
+    # get train loader and origin loader
     train_loader = torch.utils.data.DataLoader(
         torchvision.datasets.MNIST(args.dataset, train=True, download=True,
                                 transform=torchvision.transforms.Compose([
@@ -12,16 +13,7 @@ def init_dataloader(args):
                                         (0.1307,), (0.3081,))
                                 ])),
         batch_size=args.batch_size_train, shuffle=True)
-    valid_loader = torch.utils.data.DataLoader(
-        torchvision.datasets.MNIST(args.dataset, train=False, download=True,
-                                transform=torchvision.transforms.Compose([
-                                    torchvision.transforms.ToTensor(),
-                                    torchvision.transforms.Normalize(
-                                        (0.1307,), (0.3081,))
-                                ])),
-        batch_size=args.batch_size_valid, shuffle=True)
-
-    test_loader = torch.utils.data.DataLoader(
+    origin_loader = torch.utils.data.DataLoader(
         torchvision.datasets.MNIST(args.dataset, train=False, download=True,
                                 transform=torchvision.transforms.Compose([
                                     torchvision.transforms.ToTensor(),
@@ -29,7 +21,16 @@ def init_dataloader(args):
                                         (0.1307,), (0.3081,))
                                 ])),
         batch_size=args.batch_size_test, shuffle=True)
-    
+
+    # generate valid dataset and test dataset from origin dataset
+    origin_dataset = origin_loader.dataset
+    valid_split = args.valid_split
+    valid_dataset,test_dataset = Data.random_split(origin_dataset,[int(len(origin_dataset)*valid_split), len(origin_dataset) - int(len(origin_dataset)*valid_split)])
+
+    # get valid loader and test loader
+    valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size = args.batch_size_test, shuffle = True, num_workers = 0)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size = args.batch_size_test, shuffle = True, num_workers = 0)
+
     return train_loader, valid_loader, test_loader
 
 
@@ -43,10 +44,16 @@ if __name__ == "__main__":
         batch_size_train = 64
         batch_size_valid = 1000
         batch_size_test = 16
+        valid_split = 0.5
             
     args = fake_args()
-    _,_,test_loader = init_dataloader(args)
-    for data, target in test_loader:
-      print(data)
-      print(target)
-      print(type(test_loader))
+    train_loader,valid_loader,test_loader = init_dataloader(args)
+    # for data, target in test_loader:
+    #   print(data)
+    #   print(target)
+    #   print(type(test_loader))
+    print(train_loader.dataset)
+    print(valid_loader.dataset)
+    print(test_loader.dataset)
+    print(valid_loader.dataset[0][1])
+    print(test_loader.dataset[0][1])
